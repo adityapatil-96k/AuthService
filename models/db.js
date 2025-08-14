6b31c7aeff2f3ea8acf5e4da240ce92e16130b5b
adityapatil-96k <adityaramprasadambhure@gmail.com>
2025-08-14 21:39:01 +0530

    ssl added

--- models/db.js
+++ models/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // Required for Render PostgreSQL
+  },
 });
 
 pool.on('connect', () => {
@@ -18,13 +21,14 @@ pool.on('error', (err) => {
   process.exit(-1);
 });
 
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('DB connection error:', err);
-  } else {
+// Test the connection immediately
+(async () => {
+  try {
+    const res = await pool.query('SELECT NOW()');
     console.log('DB connected:', res.rows);
+  } catch (err) {
+    console.error('DB connection error:', err);
   }
-  pool.end();
-});
+})();
 
 module.exports = pool;
